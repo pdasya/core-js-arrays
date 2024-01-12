e96343c9b21ed1d61d41a5ae07c0cd64479df1e6
Pashkova Daria <dar.pashcovaa@gmail.com>
2024-01-12 17:26:29 +0900

    feat: solve distinct task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -263,8 +263,11 @@ function toStringList(inputArray) {
  *   distinct([ 1, 1, 2, 2, 3, 3, 4, 4]) => [ 1, 2, 3, 4]
  *   distinct([]) => []
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(inputArray) {
+  const uniqueSet = new Set(inputArray);
+  const uniqueArray = Array.from(uniqueSet);
+
+  return uniqueArray;
 }
 
 /**
